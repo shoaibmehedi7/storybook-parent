abc38bae20ff1465338c766ab071e372c803f8fd
Rashiq Rahman <rahmanrashiq56@gmail.com>
2023-05-19 23:03:20 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchChild from "search-child";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchChild />
+    </div>
+  );
+}
+
+export default App;
